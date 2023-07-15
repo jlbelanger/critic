89a4ffda172948a6b9a5413d218bb99a2bf7d9d3
Jenny Belanger <jbelangr@gmail.com>
2023-07-15 15:00:14 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,10 @@ source "${HOME}/Websites/infrastructure/deploy/static.sh"
 source "${HOME}/Websites/infrastructure/deploy/laravel.sh"
 
 check_git_branch
-check_git_changes
 build_static
+check_git_changes
 deploy_git
 deploy_composer
 deploy_laravel
 clear_laravel_cache
+printf "\e[0;32mDone.\n\e[0m"
