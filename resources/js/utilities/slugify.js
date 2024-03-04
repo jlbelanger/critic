ae8b071f38c1b128a856e57c7cc3ffb4a416256f
Jenny Belanger <jbelangr@gmail.com>
2024-03-03 21:02:30 -0500

    Normalize accented characters when slugifying

--- resources/js/utilities/slugify.js
+++ resources/js/utilities/slugify.js
@@ -2,7 +2,10 @@ export default (value) => {
 	if (!value) {
 		return '';
 	}
-	return value.toLowerCase()
+	return value
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.toLowerCase()
 		.replace(/ & /g, '-and-')
 		.replace(/<[^>]+>/g, '')
 		.replace(/['’.]/g, '')
